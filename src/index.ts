ed598d409a73d0b0e3f41481ea8f415987a2ddc6
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2023-07-05 17:16:56 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {
+export {
   useBanks,
   useMerchantTags,
   usePaymentRequest,
@@ -7,33 +7,11 @@ import {
   useUserPaymentDefaults,
 } from './hooks'
 
-import {
+export {
   ClientSettingsClient,
   MerchantClient,
   MoneyMoovApiClient,
   PaymentRequestClient,
 } from './clients'
 
-import * as Types from './responseTypes'
-
-const hooks = {
-  useBanks: useBanks,
-  useMerchantTags: useMerchantTags,
-  usePaymentRequest: usePaymentRequest,
-  usePaymentRequestMetrics: usePaymentRequestMetrics,
-  usePaymentRequests: usePaymentRequests,
-  useUserPaymentDefaults: useUserPaymentDefaults,
-}
-
-const clients = {
-  clientSettingsClient: ClientSettingsClient,
-  merchantClient: MerchantClient,
-  moneyMoovApiClient: MoneyMoovApiClient,
-  paymentRequestClient: PaymentRequestClient,
-}
-
-const responseTypes = {
-  ...Types,
-}
-
-export { hooks, clients, responseTypes }
+export * as Types from './responseTypes'
