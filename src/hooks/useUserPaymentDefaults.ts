dfb162405e1f8234114afd063f7b59a1281d3ab5
Donal O'Connor <donal@nofrixion.com>
2023-07-23 11:51:53 +0100

    Updated useUserPaymentDefaults hook to use react query

--- src/hooks/useUserPaymentDefaults.ts
+++ src/hooks/useUserPaymentDefaults.ts
@@ -1,33 +1,22 @@
-import { useEffect, useState } from 'react'
-import { ApiError, UserPaymentDefaults } from '../types/ApiResponses'
+import { ApiResponse, UserPaymentDefaults } from '../types/ApiResponses'
 import { ClientSettingsClient } from '../clients/ClientSettingsClient'
 import { ApiProps } from '../types/props'
+import { useQuery } from '@tanstack/react-query'
+
+const fetchUserPaymentDefaults = async (
+  apiUrl: string,
+  authToken?: string,
+): Promise<ApiResponse<UserPaymentDefaults>> => {
+  const client = new ClientSettingsClient({ apiUrl, authToken })
+  const response = await client.getUserPaymentDefaults()
+
+  return response
+}
 
 export const useUserPaymentDefaults = ({ apiUrl, authToken }: ApiProps) => {
-  const [userPaymentDefaults, setUserPaymentDefaults] = useState<UserPaymentDefaults>()
-  const [apiError, setApiError] = useState<ApiError>()
-  const [isUserPaymentDefaultsLoading, setisUserPaymentDefaultsLoading] = useState<boolean>(true)
+  const QUERY_KEY = ['UserPaymentDefaults']
 
-  useEffect(() => {
-    const fetchUserPaymentDefaults = async () => {
-      setisUserPaymentDefaultsLoading(true)
-      const client = new ClientSettingsClient({ apiUrl, authToken })
-      const response = await client.getUserPaymentDefaults()
-
-      if (response.status === 'success') {
-        setUserPaymentDefaults(response.data)
-      } else {
-        setApiError(response.error)
-      }
-      setisUserPaymentDefaultsLoading(false)
-    }
-
-    fetchUserPaymentDefaults()
-  }, [authToken, apiUrl])
-
-  return {
-    userPaymentDefaults,
-    apiError,
-    isUserPaymentDefaultsLoading,
-  }
+  return useQuery<ApiResponse<UserPaymentDefaults>, Error>(QUERY_KEY, () =>
+    fetchUserPaymentDefaults(apiUrl, authToken),
+  )
 }
